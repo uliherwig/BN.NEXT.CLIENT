63c91e1d3e4f287a56b7589d9891e7524dcd8029
uli.herwig <uli-herwig@t-online.de>
2025-03-21 13:18:04 +0100

    test

--- src/app/api/identity/test-call/route.ts
+++ src/app/api/identity/test-call/route.ts
@@ -1,12 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 export async function GET(req: NextRequest) { 
+
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!token) {
+    return NextResponse.json('No token found');
+  }
 
   const endpoint = `${process.env.IDENTITY_API_URL}/account/my-account`;
 
   const options: RequestInit = {
     method: 'GET',
-    headers: {  
+    headers: {
+      'Authorization': `Bearer ${token.accessToken}`,
       'Content-Type': 'application/json',
     },
   };
